75b285ea6c5e0d92480b95b0cf3dcce4bc58ee33
shaheereminent <shaheer_eminent@hotmail.com>
2025-11-16 22:49:25 +0500

    tipcalculator

---
+++ BandNameGenerator.py
@@ -0,0 +1,4 @@
+greet = print("Welcome to the Band Name Generator.")
+cityName = input("What's the name of the city you grew up in?\n")
+petName = input("What's your pet name?\n")
+print("Your band name could be: " + cityName + " " + petName)
---
+++ TipCalculator.py
@@ -0,0 +1,10 @@
+greeting = print("Welcome to the tip calculator!")
+
+total_bill = float(input("What was the total bill? $"))
+tip_amount = int(input("How much tip would you like to give? 10, 12 or 15? "))
+split_amount = int(input("How many people to split the bill? "))
+bill_with_tip = tip_amount / 100 * total_bill + total_bill
+each_person = bill_with_tip / split_amount 
+
+print(f"Each person should pay: ${round(each_person, 2)}")
+
